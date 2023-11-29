1b0ed6539d721f9469619f4ccc766d6329ef970c
Didnt Say Please <naodisseporfavor@gmail.com>
2023-11-28 21:03:21 -0300

    Character selection

--- UnityProject/Assets/Scenes/CharacterSelection/Scripts/CharacterSelectionController.cs
+++ UnityProject/Assets/Scenes/CharacterSelection/Scripts/CharacterSelectionController.cs
@@ -10,10 +10,25 @@ public class CharacterSelectionController : MonoBehaviour
     private List<GameObject> characters = new List<GameObject>();
 
     private GameInput gameInput;
+    private int characterSelected = 0;
+
+    private Color selectedColor = new Color(1, 1, 1);
+    private Color notSelectedColor = new Color(94f / 255f, 94f / 255f, 94f / 255f);
 
     void Start()
     {
         gameInput.Playerinmenu.Directions.performed += context => OnDirectionsPressed(context);
+
+        SetupCharactersColors();
+    }
+
+    private void SetupCharactersColors()
+    {
+        foreach (var gameCharacter in characters)
+        {
+            SetGameCharacterColor(gameCharacter, notSelectedColor);
+        }
+        SelectCharacter(0);
     }
 
     private void OnDirectionsPressed(InputAction.CallbackContext context)
@@ -21,6 +36,39 @@ public class CharacterSelectionController : MonoBehaviour
         var direction = context.ReadValue<Vector2>();
 
         Debug.Log("X: "+direction.x + " y: "+ direction.y);
+
+        if (direction.x > 0 && characterSelected != 1)
+        {
+            SelectCharacter(1);
+        }
+        else if (direction.x < 0 && characterSelected != 0)
+        {
+            SelectCharacter(0);
+        }
+    }
+
+    private void SelectCharacter(int characterSelected)
+    {
+        this.characterSelected = characterSelected;
+
+        var character = characters[characterSelected];
+
+        foreach(var gameCharacter in characters)
+        {
+            SetGameCharacterColor(gameCharacter, notSelectedColor);
+        }
+
+        SetGameCharacterColor(character, selectedColor);
+    }
+
+    private void SetGameCharacterColor(GameObject gameCharacter, Color color)
+    {
+        var spriteRenderer = gameCharacter.GetComponent<SpriteRenderer>();
+
+        if(spriteRenderer != null)
+        {
+            spriteRenderer.color = color;
+        }
     }
 
     private void OnEnable()
